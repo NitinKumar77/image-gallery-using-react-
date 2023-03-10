d9582383efcd90cdacc12828cdc1191f4676eed3
Nitin <nkmehra77@gmail.com>
2023-03-10 20:31:17 +0530

    fd

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import SearchFilter from "./Components/SearchFilter";
 import Header from "./Components/Header";
 import Pagination from "./Components/Pagination";
 import ShowImages from "./Components/ShowImages";
-
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <SearchFilter />
       <ShowImages />
